5de3cd8d03a87b1b4a8e3d4373f8ca0d6330ef14
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-05-16 10:00:08 +0200

    it 4 and 5 solved

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,10 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+	return moviesArray.map(movie => movie.director)
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {}
@@ -10,10 +13,36 @@ function howManyMovies(moviesArray) {}
 function scoresAverage(moviesArray) {}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+	const justDrama = moviesArray.filter(movie => movie.genre.includes('Drama'))
+
+	if(justDrama.length === 0) {
+		return 0
+	}
+
+	const dramaAverage = justDrama.reduce((accumulator, currentValue) => {
+		return accumulator + currentValue.score
+	}, 0) / justDrama.length
+
+	const decimals = Math.round(dramaAverage * 100) / 100
+
+	return decimals
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+	const copy = [...moviesArray]
+
+	copy.sort((a, b) => {
+		if (a.year === b.year) {
+			// return a.title.localeCompare(b.title,'en', { sensitivity: 'base'})
+			return a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+		}
+		return a.year - b.year
+	})
+
+	return copy
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
